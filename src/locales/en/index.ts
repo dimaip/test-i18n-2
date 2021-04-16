c80bd78024d95bafdf9af2bc60a7f288a348fec3
Dmitri Pisarev <dimaip@gmail.com>
2021-04-16 10:37:30 +0300

    Sample translations

---
+++ src/locales/en/index.ts
@@ -0,0 +1,69 @@
+import common from './common.json'
+import menu from './menu.json'
+import offerer from './offerer.json'
+import offerers from './offerers.json'
+import profile from './profile.json'
+import requester from './requester.json'
+import offererEdit from './offererEdit.json'
+import requesterEdit from './requesterEdit.json'
+import createAccountForm from './createAccountForm.json'
+import aboutAccountForm from './aboutAccountForm.json'
+import aboutCompanyForm from './aboutCompanyForm.json'
+import brandAssetsForm from './brandAssetsForm.json'
+import myOpportunities from './myOpportunities.json'
+import opportunities from './opportunities.json'
+import opportunity from './opportunity.json'
+import requesters from './requesters.json'
+import imageUpload from './imageUpload.json'
+import requesterHomePage from './requesterHomePage.json'
+import offererHomePage from './offererHomePage.json'
+import modals from './modals.json'
+import createOpportunity from './createOpportunity.json'
+import offererInvite from './offererInvite.json'
+import footer from './footer.json'
+import loginForm from './loginForm.json'
+import offererAssetsForm from './offererAssetsForm.json'
+import feedback from './feedback.json'
+import forgotPasswordForm from './forgotPasswordForm.json'
+import ssoLogin from './ssoLogin.json'
+import posts from './posts.json'
+import passwordReset from './passwordReset.json'
+import editOpportunity from './editOpportunity.json'
+import editForm from './editForm.json'
+import landingPage from './landingPage.json'
+
+export default {
+  common,
+  menu,
+  offerer,
+  offerers,
+  profile,
+  requester,
+  offererEdit,
+  requesterEdit,
+  myOpportunities,
+  createAccountForm,
+  aboutAccountForm,
+  aboutCompanyForm,
+  brandAssetsForm,
+  opportunities,
+  opportunity,
+  requesters,
+  imageUpload,
+  requesterHomePage,
+  offererHomePage,
+  modals,
+  createOpportunity,
+  offererInvite,
+  footer,
+  loginForm,
+  offererAssetsForm,
+  feedback,
+  forgotPasswordForm,
+  ssoLogin,
+  posts,
+  passwordReset,
+  editOpportunity,
+  editForm,
+  landingPage,
+}
